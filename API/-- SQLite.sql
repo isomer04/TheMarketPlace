8154db3945935a113b3b2c14ba603defa0aee0ba
isomer04 <isomer008@gmail.com>
2023-06-20 00:03:46 -0400

    Fixed the null exception error

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (5, "Tom");
+
+INSERT INTO Users (Id, UserName)
+VALUES (6, "Jack");
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,6 +9,6 @@ namespace API.Data
         {
         }
 
-        public DbSet<AppUser> Users { get;}
+        public DbSet<AppUser> Users { get; set; }
     }
 }
