40ac83593e377df0d09997f53af793debdb26dd1
isomer04 <isomer008@gmail.com>
2023-06-21 00:09:12 -0400

    Worked on making HTTP requests in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TheMarketPlace';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void{
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request has completed")
+    })
+  }
 }
