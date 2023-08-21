6fdcf19385a799d9bf19dfa3a034fd727a7af980
isomer04 <isomer008@gmail.com>
2023-08-20 23:24:46 -0400

    Added helper classes for pagination

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -7,7 +7,7 @@ namespace API.Helpers
     {
         public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize){
             CurrentPage = pageNumber;
-            TotalPages = (int) Math.Ceiling(count / (double)pageSize);
+            TotalPages = (int) Math.Ceiling(count/(double)pageSize);
             PageSize = pageSize;
             TotalCount = count;
             AddRange(items);
@@ -21,8 +21,10 @@ namespace API.Helpers
         public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
         {
             var count = await source.CountAsync();
-            var items =  await source.Skip(((pageNumber -1 ) * pageSize).Take(pageSize).ToListAsync();
+            var items =  await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PagedList<T>(items, count, pageNumber, pageSize);
         }
     }
+
+    
 }
