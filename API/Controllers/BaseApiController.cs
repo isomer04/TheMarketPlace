ac181ca1db63bb8ee4919fc65ef4092060d91311
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-09-04 22:27:10 -0400

    Added an action filter

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,9 +1,11 @@
 
 
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")] // GET /api/users
     public class BaseApiController: ControllerBase
