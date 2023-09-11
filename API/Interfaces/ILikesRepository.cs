0e354b9284cd3bea014f33402c2ffc2a9acf8726
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-09-11 14:56:51 -0400

    Worked with controller methods for the likes

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -9,7 +9,7 @@ namespace API.Interfaces
 {
     public interface ILikesRepository
     {
-        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
         Task<AppUser> GetUserWithLikes(int userId);
         Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
     }
