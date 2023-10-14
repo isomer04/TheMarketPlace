2019f0d80965e6ec6a1091c6c0db9d2aef9fb37b
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-10-14 11:18:16 -0400

    Worked on setting up the message repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -23,6 +23,7 @@ namespace API.Extensions
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRespository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
 
             return services;
         }
