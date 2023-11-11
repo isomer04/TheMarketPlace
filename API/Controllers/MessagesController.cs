be75be8a93fb5f0bf383fd58ae7fd11bf4aa2c61
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-11-10 23:37:03 -0500

    Worked on getting the message thread for 2 users

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -64,6 +64,15 @@ namespace API.Controllers
             return messages;
         }
 
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUserName = User.GetUsername();
+            return Ok(await _messageRepository.GetMessageThread(currentUserName, username));
+        }
+
+
+
 
 
     }
