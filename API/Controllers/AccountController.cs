8fae935c18220c177644526420256775d03ebce8
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-04 09:39:13 -0500

    Updated the account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,13 +16,13 @@ namespace API.Controllers
     public class AccountController: BaseApiController
     {
 
-        private readonly DataContext _context;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper){
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper){
+            _userManager = userManager;
             _mapper = mapper;
-            _context = context;
             _tokenService = tokenService;
         }
 
@@ -31,16 +32,12 @@ namespace API.Controllers
             if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             
             var user = _mapper.Map<AppUser>(registerDto);
-
-
             
-                user.UserName = registerDto.Username.ToLower();
+            user.UserName = registerDto.Username.ToLower();
 
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if(!result.Succeeded) return BadRequest(result.Errors);
             
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
             return new UserDto{
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
@@ -53,11 +50,15 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
+            var user = await _userManager.Users
                 .Include(p => p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username); 
 
             if(user == null) return Unauthorized("invalid username");
+            
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if(!result) return Unauthorized("invalid password");
 
             return new UserDto{
                 Username = user.UserName,
@@ -69,7 +70,7 @@ namespace API.Controllers
         }
 
         private async Task<bool> UserExists(string username){
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
         
     }
