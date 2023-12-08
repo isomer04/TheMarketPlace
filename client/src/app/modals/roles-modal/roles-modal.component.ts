6073c28444f7b77e1a4e0d2f9408ca63d8cfd554
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-08 17:11:28 -0500

    Worked on setting up modals

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-modal',
+  templateUrl: './roles-modal.component.html',
+  styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent implements OnInit {
+  title = '';
+  list: any;
+  closeBtnName = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
