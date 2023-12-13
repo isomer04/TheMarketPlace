bfa17eaaa273a839a7df42ce901988f2563fc5ef
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-12 19:52:27 -0500

    Sent messages via the hub

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -5,7 +5,7 @@ import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 import { Message } from '../_models/message';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { User } from '../_models/user';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -32,6 +32,14 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
     })
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread$.pipe(take(1)).subscribe({
+        next: messages => {
+          this.messageThreadSource.next([...messages,message])
+        }
+      })
+    })
   }
 
   stopHubConnection(){
@@ -57,6 +65,7 @@ export class MessageService {
   }
 
   sendMessage(username: string, content: string){
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content})
+    return this.hubConnection?.invoke('SendMessage', {recipientUsername: username, content})
+    .catch(error => console.log(error));
   }
 }
