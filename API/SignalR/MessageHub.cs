a37e0b4e20b5d818146e44c492b7be96f034ebf9
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-15 23:52:47 -0500

    Optimized the messages

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -30,7 +30,9 @@ namespace API.SignalR
             var otherUser = httpContext.Request.Query["user"];
             var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await AddToGroup(groupName);
+            var group = await AddToGroup(groupName);
+
+            await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
 
             var messages = await _messageRepository
                 .GetMessageThread(Context.User.GetUsername(), otherUser);
@@ -40,7 +42,8 @@ namespace API.SignalR
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {   
-            await RemoveFromMessageGroup();
+            var group = await RemoveFromMessageGroup();
+            await Clients.Group(group.Name).SendAsync("UpdatedGroup");
             await base.OnDisconnectedAsync(exception);
         }
 
@@ -93,7 +96,7 @@ namespace API.SignalR
             return stringCompare ? $"{caller} - {other}": $"{other} - {caller}";
         }
 
-        public async Task<bool> AddToGroup(string groupName){
+        public async Task<Group> AddToGroup(string groupName){
             var group = await _messageRepository.GetMessageGroup(groupName);
             var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
 
@@ -105,14 +108,20 @@ namespace API.SignalR
 
             group.Connections.Add(connection);
 
-            return await _messageRepository.SaveAllAsync();
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to add to group");
         }
 
-        private async Task RemoveFromMessageGroup()
+        private async Task<Group> RemoveFromMessageGroup()
         {
-            var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+            var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+            var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+
             _messageRepository.RemoveConnection(connection);
-            await _messageRepository.SaveAllAsync();
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to remove from group");
         }
     }
 }
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -6,46 +6,65 @@ import { Message } from '../_models/message';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { User } from '../_models/user';
 import { BehaviorSubject, take } from 'rxjs';
+import { Group } from '../_models/group';
+import { BusyService } from './busy.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   baseUrl = environment.apiUrl;
-  hubUrl =  environment.hubUrl;
+  hubUrl = environment.hubUrl;
   private hubConnection?: HubConnection;
-  private messageThreadSource = new BehaviorSubject<Message[]>([]);
-  messageThread$ = this.messageThreadSource.asObservable();
+  private messageThreadSouce = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSouce.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private busyService: BusyService) { }
 
-  createHubConnection(user: User, otherUsername: string){
+  createHubConnection(user: User, otherUsername: string) {
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
         accessTokenFactory: () => user.token
       })
-
       .withAutomaticReconnect()
       .build();
+    this.hubConnection.start()
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
-    this.hubConnection.start().catch(error => console.log(error));
     this.hubConnection.on('ReceiveMessageThread', messages => {
-      this.messageThreadSource.next(messages);
+      this.messageThreadSouce.next(messages);
     })
 
     this.hubConnection.on('NewMessage', message => {
       this.messageThread$.pipe(take(1)).subscribe({
         next: messages => {
-          this.messageThreadSource.next([...messages,message])
+          this.messageThreadSouce.next([...messages, message])
         }
       })
     })
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some(x => x.username === otherUsername)) {
+        this.messageThread$.pipe(take(1)).subscribe({
+          next: messages => {
+            messages.forEach(message => {
+              if (!message.dateRead) {
+                message.dateRead = new Date(Date.now())
+              }
+            })
+            this.messageThreadSouce.next([...messages]);
+          }
+        })
+      }
+    })
   }
 
-  stopHubConnection(){
-    if(this.hubConnection){
-      this.hubConnection.stop();
-
+  stopHubConnection() {
+    if (this.hubConnection) {
+      this.messageThreadSouce.next([]);
+      this.hubConnection?.stop();
     }
   }
 
@@ -59,13 +78,12 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection?.invoke('SendMessage', { recipientUsername: username, content })
+      .catch(error => console.log(error));
+  }
 
   deleteMessage(id: number) {
     return this.http.delete(this.baseUrl + 'messages/' + id);
-  }
-
-  sendMessage(username: string, content: string){
-    return this.hubConnection?.invoke('SendMessage', {recipientUsername: username, content})
-    .catch(error => console.log(error));
   }
 }
