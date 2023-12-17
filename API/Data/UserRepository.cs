050d6f4676cee84444baba7e5b50afe5c2e90899
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-16 23:01:04 -0500

    Implemented the unit of work

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -37,10 +37,7 @@ namespace API.Data
             .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
+
 
         public void Update(AppUser user)
         {
