b5d02deba84043b214f38a8f229e4cff185ef16f
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-12-24 11:06:30 -0500

    Dockerized the app

--- API/Program.cs
+++ API/Program.cs
@@ -31,7 +31,6 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder
-
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
@@ -44,23 +43,25 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapControllers();
-app.MapHub<PresenceHub>("hubs/presense");
+app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
 app.MapFallbackToController("Index", "Fallback");
 
-using var scope =  app.Services.CreateScope();
-var services =  scope.ServiceProvider;
-try {
-    var context =  services.GetRequiredService<DataContext>();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
-    var roleManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
     await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
-} catch (Exception ex){
-    var logger = services.GetService<ILogger<Exception>>();
-    logger.LogError(ex, "An Error occurred duing migration");
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
-
